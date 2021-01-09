25dd4f078972ab9c6150a43f615e873c62fad25f
Даниил Иванов <65729958+AliensWithdrawPeople@users.noreply.github.com>
2021-01-09 17:32:57 +0500

    Fix and Update
    No more bracketEQ! Now I know about instance! Mostly fixes are invisible/

--- Base.hs
+++ Base.hs
@@ -1,43 +1,32 @@
+-- Module      : Symbols.Base
+-- Copyright   : (c) Daniel Ivanov 2021
+-- License     : MIT
+-- Maintainer  : daniilivanov1606@gmail.com
+-- Stability   : experimental
+-- Portability : requires GHC>7 extensions
+
 module Base 
 ( parser
-, evaluator) where
+, evaluator
+, exprShow) where
 
 import Data.List ( groupBy, all )
 import Data.Char ( isDigit, isAlpha )
 
-data Token = Atom Var | Func (Float -> Float) | Op (Float -> Float -> Float ) | Bracket Char
+data Token = Atom Part | Func (Float -> Float) | Op (Float -> Float -> Float ) | Bracket Char 
+data Part = Number Float | Var String   
 
-data Var = AtomNum Float | AtomVar String
+instance Eq Token where
+      Bracket '(' == Bracket '(' = True 
+      Bracket ')' == Bracket ')' = True  
+      _ == _ = False
+      
+instance Show Token where
+      show x = tokenShow x
 
 parser :: String -> [Token]
 parser = map parseToken . filter (/= " ") . negNum [] . grouper
       where grouper =  groupBy (\x y -> (isDigit x && isDigit y) || (isAlpha x && isAlpha y) || (isDigit x && y == '.'))
-
-negNum :: [String] -> [String] -> [String]
-negNum out ("-" : y : xs) = negNum (("-" ++ y) : "+" : "0" : out) xs
-negNum out (x : xs) = negNum (x : out) xs
-negNum out [] = reverse out
-
-bracketEQ :: Char -> Token -> Bool 
-bracketEQ a (Bracket x) = a == x
-bracketEQ a t = False
-
-opsPriority :: Token -> Int  
-opsPriority (Op op)
-      | op 2 3 == 2 ** 3 = 3
-      | op 2 3 == 2 * 3 = 2
-      | op 2 3 == 2 / 3 = 2
-      | op 2 3 == 2 + 3 = 1
-      | op 2 3 == 2 - 3 = 1
-opsPriority (Func f) = 4
-opsPriority x = 0
-
-toNum :: Token -> Float 
-toNum (Atom (AtomNum x)) = x
-
-varCheck :: Token -> Bool 
-varCheck (Atom (AtomVar x)) = True
-varCheck x = False 
 
 parseToken :: [Char] -> Token
 parseToken x 
@@ -51,27 +40,27 @@ parseToken x
       | x == "sin" = Func sin
       | x == "(" =  Bracket '('
       | x == ")" =  Bracket ')'
-      | all isAlpha x = (Atom . AtomVar) x
-      | otherwise   = (Atom . AtomNum . read) x
+      | all isAlpha x = (Atom . Var) x
+      | otherwise   = (Atom . Number . read) x
 
 -- shuntingYard :: input queue -> output queue -> operators stack -> output queue in rpn
 shuntingYard :: [Token] -> [Token] -> [Token] -> [Token]
-shuntingYard (Atom (AtomNum n) : xs) out ops = shuntingYard xs (Atom (AtomNum n) : out) ops
-shuntingYard (Func f : Atom (AtomNum n) : xs) out ops = shuntingYard xs ((Atom . AtomNum . f) n : out) ops
+shuntingYard (Atom (Number n) : xs) out ops = shuntingYard xs (Atom (Number n) : out) ops
+shuntingYard (Func f : Atom (Number n) : xs) out ops = shuntingYard xs ((Atom . Number . f) n : out) ops
 shuntingYard (Func f : xs) out ops = shuntingYard xs out (Func f : ops)
 shuntingYard (Op op1 : xs) out (x : ops) = if (opsPriority . Op) op1 <= opsPriority x then shuntingYard (Op op1 : xs) (x : out) ops
                                                 else shuntingYard xs out (Op op1 : x : ops)
 shuntingYard (Op op1 : xs) out ops = shuntingYard xs out (Op op1 : ops)
 shuntingYard (Bracket '(' : xs) out ops = shuntingYard xs out (Bracket '(' : ops)
-shuntingYard (Bracket ')' : xs) out (x : ops) = if bracketEQ '(' x then shuntingYard xs out ops
+shuntingYard (Bracket ')' : xs) out (x : ops) = if x == Bracket '(' then shuntingYard xs out ops
                                                 else shuntingYard (Bracket ')' : xs) (x : out) ops
 shuntingYard [] out (x : ops) = shuntingYard [] (x : out) ops
 shuntingYard [] out [] = reverse out
 
 solveRPN :: [Token] -> Token 
 solveRPN = head . foldl foldingFunction []  
-    where   foldingFunction (Atom (AtomNum x1) : xs) (Func func) = (Atom . AtomNum . func) x1 : xs
-            foldingFunction (Atom (AtomNum x1) : Atom (AtomNum x2) : xs) (Op op) = (Atom . AtomNum) (op x2 x1) : xs
+    where   foldingFunction (Atom (Number x1) : xs) (Func func) = (Atom . Number . func) x1 : xs
+            foldingFunction (Atom (Number x1) : Atom (Number x2) : xs) (Op op) = (Atom . Number) (op x2 x1) : xs
             foldingFunction xs ys = ys : xs
 
 evaluator :: String -> Maybe Float 
@@ -80,6 +69,44 @@ evaluator xs
       | (any varCheck . parser) xs = Nothing  
       | otherwise  = Just $ (toNum . solveRPN) $ shuntingYard (parser xs) [] []
 
--- main :: IO ()     
--- main = do
---      print ( evaluator "ln(-1+2) + 2 + 2*2^2 + 53 + x")
+
+-- helper functions
+
+tokenShow :: Token -> String 
+tokenShow (Op op)
+      | op 2 3 == 2 ** 3 = " ^ "
+      | op 2 3 == 2 * 3 = " * "
+      | op 2 3 == 2 / 3 = " / "
+      | op 2 3 == 2 + 3 = " + "
+      | op 2 3 == 2 - 3 = " - "
+tokenShow (Func f)
+      | f 2 == log 2 = " ln"
+      | f 2 == sin 2 = " sin"
+tokenShow (Bracket x) = " " ++ (x : " ")
+tokenShow (Atom (Number x)) = show x
+tokenShow (Atom (Var x)) = x
+
+negNum :: [String] -> [String] -> [String]
+negNum out ("-" : y : xs) = negNum (("-" ++ y) : "+" : "0" : out) xs
+negNum out (x : xs) = negNum (x : out) xs
+negNum out [] = reverse out
+
+opsPriority :: Token -> Int  
+opsPriority (Op op)
+      | op 2 3 == 2 ** 3 = 3
+      | op 2 3 == 2 * 3 = 2
+      | op 2 3 == 2 / 3 = 2
+      | op 2 3 == 2 + 3 = 1
+      | op 2 3 == 2 - 3 = 1
+opsPriority (Func f) = 4
+opsPriority x = 0
+
+toNum :: Token -> Float 
+toNum (Atom (Number x)) = x
+
+varCheck :: Token -> Bool 
+varCheck (Atom (Var x)) = True
+varCheck x = False
+
+exprShow :: [Token] -> String 
+exprShow = concatMap show
